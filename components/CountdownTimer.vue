cbe7f65d0189a9d685f85a3f8b587751e2b410f2
TillMac <danny890308@gmail.com>
2024-11-26 16:52:24 +0800

    fix: 根據頁數與 isRunning 與否來判定計時器是否顯示

--- components/CountdownTimer.vue
+++ components/CountdownTimer.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="fixed bottom-2 right-2 flex items-center gap-2">
     <button 
-      v-if="!isRunning" 
+      v-if="!isRunning && $nav.currentPage === 1" 
       @click="startTimer"
       class="bg-[#00AAA4] text-white px-2 py-1 rounded hover:bg-[#008884]"
     >
       Start Timer
     </button>
-    <div :class="`bg-white/80 px-2 py-1 rounded ${countdownDuration < 60 ? 'text-red-500' : ''} ${isRunning && countdownDuration < 60 ? 'animate-pulse' : ''}`">
+    <div v-if="isRunning" :class="`bg-white/80 px-2 py-1 rounded ${countdownDuration < 60 ? 'text-red-500' : ''} ${isRunning && countdownDuration < 60 ? 'animate-pulse' : ''}`">
       {{ Math.floor(countdownDuration / 60) }}:{{ (countdownDuration % 60).toString().padStart(2, '0') }}
     </div>
   </div>
